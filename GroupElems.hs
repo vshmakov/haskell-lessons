051bd4ad111b84d596e451ab52a28b7792493da1
VadimShmakov <shmakovvadim@mail.ru>
2022-01-15 20:07:55 +0500

    Grouped elems by takeWhile / dropWhile

--- GroupElems.hs
+++ GroupElems.hs
@@ -1,5 +1,4 @@
 groupElems :: Eq a => [a] -> [[a]]
-groupElems as = map (uncurry replicate) (countElems as)
-  where
-    countElems (a : as) (b : bs)
-      | snd b == a = (a, snd b + 1) : bs
+groupElems [] = []
+groupElems (a : as) = (takeWhile ea as ++ [a]) : groupElems (dropWhile ea as)
+  where ea = (a==)
