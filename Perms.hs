6f1b1dc5404caee34715e7d79c591f4a94dae40e
VadimShmakov <shmakovvadim@mail.ru>
2022-01-24 22:52:41 +0500

    Solved perms

--- Perms.hs
+++ Perms.hs
@@ -1,2 +1,8 @@
+import Data.List
+
+shifts (a : as) = map (\i -> take i as ++ [a] ++ drop i as) [0..length as]
+
 perms :: [a] -> [[a]]
-perms (a : as) = undefined
+perms [] = []
+perms [a] = [[a]]
+perms (a : as) = sortBy (\x -> \y -> compare $ sum x sum y) $ concatMap (\xs -> shifts ([a] ++ xs)) (perms as)
