ba0b708b1b6c36da062f0880eaea8a5752183ac2
VadimShmakov <shmakovvadim@mail.ru>
2022-01-29 12:52:28 +0500

    Solved perms

--- Perms.hs
+++ Perms.hs
@@ -1,21 +1,6 @@
-qsort :: Ord a => [a] -> [a]
-qsort [] = []
-qsort as@(a : _) = qsort (filterWith (<)) ++ filterWith (==) ++ qsort (filterWith (>))
-  where filterWith compare = filter ((flip compare) a) as
-
-shifts (a : as) = map (\i -> take i as ++ [a] ++ drop i as) [0..length as]
-
-s :: [Int] -> Int
-s a = read (concat (map show a))
-
-sort a = qsort (map s a)
-
-readInt :: Char -> Int
-readInt a = read [a]
-
-toArrays :: [Int] -> [[Int]]
-toArrays = map (\a -> map readInt (show a))
-
-perms [] = []
-perms [a] = [[a]]
-perms (a : as) = concatMap (\xs -> shifts ([a] ++ xs)) (perms as)
+perms :: [a] -> [[a]]
+perms []     = [[]]
+perms (x:xs) = [y | p <- perms xs, y <- interleave p]
+  where
+    interleave []     = [[x]]
+    interleave (y:ys) = (x:y:ys) : map (y:) (interleave ys)
