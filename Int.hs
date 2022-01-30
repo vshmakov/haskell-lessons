0adc2abe83997950814c0c6f9450feb2582dc989
VadimShmakov <shmakovvadim@mail.ru>
2022-01-30 20:24:51 +0500

    [integration] Solved with generator

--- Int.hs
+++ Int.hs
@@ -1,10 +1,5 @@
-acc i f a h r
-  | i == 0 = r
-  | otherwise = acc (i - 1) f a h (r + f (a + h * i))
-
 integration :: (Double -> Double) -> Double -> Double -> Double
-integration f a b =
-  let
+integration f a b = h / 2 * (f a + f b + sum [f (a + h * x) * 2 | x <- [1..s - 1]])
+  where
     s = 1000
-    h = (b -a) / s
-  in h / 2 * (f a + f b + acc (s - 1) f a h 0 * 2)
+    h = (b - a) / s
